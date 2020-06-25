cfc1429a5a00606a5afcd557a233ee45d1f85949
Ruben Bartelink <ruben@bartelink.com>
2020-06-25 09:34:47 +0100

    Remove problematic .Name usage

--- src/FsKafka0/FsKafka.fs
+++ src/FsKafka0/FsKafka.fs
@@ -491,7 +491,7 @@ type BatchedConsumer private (inner : Consumer<string, string>, task : Task<unit
         let consumer, unsubLog = ConsumerBuilder.WithLogging(log, config.inner, onRevoke = onRevoke)
         let cts = new CancellationTokenSource()
         let triggerStop () =
-            log.Information("Consuming... Stopping {name:l}", consumer.Name)
+            log.Information("Consuming... Stopping {name:l}", config.inner.ClientId)
             cts.Cancel()
             unsubLog ()
         let task = ConsumerImpl.mkBatchedMessageConsumer log config.buffering cts.Token consumer partitionHandler |> Async.StartAsTask
