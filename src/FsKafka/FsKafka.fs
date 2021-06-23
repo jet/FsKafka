e5d9978f3bdd554683e9ffbbd9bdf8c59fcd04cc
Asti <rahul.ramakumar@walmart.com>
2021-06-23 19:52:29 +0530

    Add in reverted

--- src/FsKafka/FsKafka.fs
+++ src/FsKafka/FsKafka.fs
@@ -171,12 +171,13 @@ type BatchedProducer private (inner : IProducer<string, string>, topic : string)
     ///    Note that the delivery and/or write order may vary from the supplied order unless `maxInFlight` is 1 (which massively constrains throughput).
     ///    Thus it's important to note that supplying >1 item into the queue bearing the same key without maxInFlight=1 risks them being written out of order onto the topic.
     /// </remarks>
-    member __.ProduceBatch(messageBatch : Message<_, _>[]) : Async<DeliveryReport<string,string>[]> = async {
+    member __.ProduceBatch(keyValueBatch : Message<_, _>[]) : Async<DeliveryReport<string,string>[]> = async {
+        if Array.isEmpty keyValueBatch then return [||] else
 
         let! ct = Async.CancellationToken
 
         let tcs = TaskCompletionSource<DeliveryReport<_,_>[]>()
-        let numMessages = messageBatch.Length
+        let numMessages = keyValueBatch.Length
         let results = Array.zeroCreate<DeliveryReport<_,_>> numMessages
         let numCompleted = ref 0
 
@@ -191,7 +192,7 @@ type BatchedProducer private (inner : IProducer<string, string>, topic : string)
                 results.[i - 1] <- m
                 if i = numMessages then tcs.TrySetResult results |> ignore 
 
-        for message in messageBatch do
+        for message in keyValueBatch do
             inner.Produce(topic, message, deliveryHandler=handler)
 
         inner.Flush(ct)
